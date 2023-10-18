59886d20b49424a8e5e35f8a81624f5d2029f3fa
Vinnixz <131690683+Vinnixz@users.noreply.github.com>
2023-10-18 17:47:25 +0000

    criando ranking

--- projetos.php
+++ projetos.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Conteúdo de Trabalho</title>
-   <link rel="stylesheet" type="text/css" href="projetos.css">
+    <link rel="stylesheet" type="text/css" href="projetos.css">
 </head>
 <body>
     <header>
@@ -13,22 +13,17 @@
         <h2>Nossos Projetos</h2>
         <p>Aqui estão alguns dos projetos em que estamos trabalhando atualmente:</p>
         <ul>
-            <li>Projeto A: Descrição do Projeto A</li>
-            <li>Projeto B: Descrição do Projeto B</li>
-            <li>Projeto C: Descrição do Projeto C</li>
-            <!-- Adicione mais projetos conforme necessário -->
+            <li>
+                <form method="POST" action="ranking.php">
+                    <button type="submit">Ranking das Series</button>
+                </form>
+            </li>
         </ul>
     </section>
 
     <section>
         <h2>Desafios Recentes</h2>
         <p>Enfrentamos desafios significativos, e estamos trabalhando duro para superá-los. Aqui estão alguns deles:</p>
-        <ul>
-            <li>Desafio 1: Descrição do Desafio 1</li>
-            <li>Desafio 2: Descrição do Desafio 2</li>
-            <li>Desafio 3: Descrição do Desafio 3</li>
-            <!-- Adicione mais desafios conforme necessário -->
-        </ul>
     </section>
 
     <footer>
---
+++ ranking.php
@@ -0,0 +1,3 @@
+<?php
+  echo ("este eh o ranking")
+?>
