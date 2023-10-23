261a3fe16da7a49e92d4ea7444c932f6c0d673c1
Vinnixz <131690683+Vinnixz@users.noreply.github.com>
2023-10-23 16:30:43 +0000

    atualizando css do site

--- index.php
+++ index.php
@@ -3,90 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
     <title>Login - Netflix</title>
-    <style>
-
-      @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@500&display=swap');
-
-        body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            background-color: black;
-        }
-
-       h2 {
-        font-family: 'Roboto Slab', sans-serif;
-        font-weight: 300; 
-        color: black;
-       }
-        .login-container {
-            background-color: white; 
-            padding: 20px;
-            border-radius: 10px;
-            text-align: center;
-        }
-
-    
-        form {
-            width: 300px;
-            margin: 5px 0px;
-            padding: 20px;
-            color: white;
-            text-align: center;
-            
-           
-        }
-
-        form input[type="text"],
-        form input[type="password"] {
-            width: 100%;
-            padding: 10px;
-            margin: 10px 0;
-            border: 1px solid grey; 
-            border-radius: 5px;
-            width: 100%;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
-
-        form button {
-            width: 100%;
-            padding: 10px;
-            background-color: #e50914; 
-            border: none;
-            border-radius: 5px;
-            color: black;
-            font-weight: bold;
-            cursor: pointer;
-
-        form genero {
-          position: left;
-        }
-        }
-        
-    </style>
 </head>
 <body>
-  <style>
-    body {
-      background-image: url("https://t.ctcdn.com.br/xzJNU6tuo-MVcwFd3yxx52bkURs=/768x432/smart/i576969.png");
-      background-repeat: none;
-      width
-    }
-  </style>
-    <div class="login-container">
-    
-        <h2>Cadastro</h2>
-        <form method="POST" action="start.php">
-            <label for="email">Email:</label>
-            <input type="text" id="email" name="email" required placeholder="Digite seu email:">
-            <label for="senha">Senha (3 primeiros dígitos do CPF):</label>
-            <input type="password" id="senha" name="senha" required placeholder="3 primeiros digitos do cpf">
-            <button type="submit">Entrar</button>
-        </form>
-    </div>
+  <div class="login-container">
+      <h2>Cadastro</h2>
+      <form method="POST" action="processa_cadastro.php">
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" required placeholder="Digite seu email">
+
+          <label for="senha">Senha (3 primeiros dígitos do CPF):</label>
+          <input type="password" id="senha" name="senha" required placeholder="3 primeiros dígitos do CPF">
+
+          <label for="confirmar-senha">Confirme a senha:</label>
+          <input type="password" id="confirmar-senha" name="confirmar-senha" required placeholder="Confirme a senha">
+
+          <label for="idade-confirmada";">Declaro que tenho mais de 18 anos:</label>
+          <input type="checkbox" id="idade-confirmada" name="idade-confirmada" required>
+
+          <label for="genero">Gênero:</label>
+          <select id="genero" name="genero">
+              <option value="masculino">Masculino</option>
+              <option value="feminino">Feminino</option>
+              <option value="outro">Outro</option>
+          </select>
+
+          <button type="submit">Cadastrar</button>
+      </form>
+  </div>
 </body>
 </html>
---
+++ processa_cadastro.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/processa_cadastro.css">
+    <title>Cadastro - Netflix</title>
+</head>
+<body>
+  <div class="message-container">
+      <?php
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          $email = $_POST["email"];
+          $senha = $_POST["senha"];
+          $confirmarSenha = $_POST["confirmar-senha"];
+          $idadeConfirmada = isset($_POST["idade-confirmada"]);
+          $genero = $_POST["genero"];
+
+          if ($senha != $confirmarSenha) {
+              echo "As senhas não coincidem. Por favor, tente novamente.";
+          } else {
+              if (!$idadeConfirmada) {
+                  echo "Você precisa confirmar que tem 18 anos ou mais para continuar.";
+              } else {
+                  header("Location: start.php");
+                  exit;
+              }
+            }
+          }
+      
+      ?>
+  </div>
+</body>
+</html>
